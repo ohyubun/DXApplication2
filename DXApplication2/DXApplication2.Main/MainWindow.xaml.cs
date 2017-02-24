ed39da6eb8484f100893521598be4cfe88840efc
ohyubun <ohyubun@gmail.com>
2017-02-24 22:31:35 +0800

    Add project files.

---
+++ DXApplication2/DXApplication2.Main/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using DevExpress.Xpf.Ribbon;
+
+namespace DXApplication2.Main
+{
+    public partial class MainWindow : DXRibbonWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ DXApplication2/DXApplication2.Modules/Views/ModuleView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace DXApplication2.Modules.Views
+{
+    public partial class ModuleView : UserControl
+    {
+        public ModuleView()
+        {
+            InitializeComponent();
+        }
+    }
+}
